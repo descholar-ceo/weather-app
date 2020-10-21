188d45606337053d28f37a75a76b9d8c3b63925b
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-21 22:47:33 +0200

    Add some APIs info

--- src/handlers/handle-search.js
+++ src/handlers/handle-search.js
@@ -1,5 +1,21 @@
 const handleSearch = (inputField) => {
   const searchQuery = inputField.value;
+  fetch('https://openweatherapp.p.rapidapi.com/', {
+    mode: 'cors',
+    method: 'POST',
+    headers: {
+      'x-rapidapi-host': 'openweatherapp.p.rapidapi.com',
+      'x-rapidapi-key': 'bb3ebde477msh2bc5f9123f0cc95p1d6922jsn4f0b847ad367',
+      'content-type': 'application/x-www-form-urlencoded',
+    },
+    body: {},
+  })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err);
+    });
   console.log(searchQuery);
 };
 
