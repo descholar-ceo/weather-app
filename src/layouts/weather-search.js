7faf668785c73f61b54158594b91cfc35b5cf8a2
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-21 15:17:37 +0200

    Fix stylelint error

--- src/layouts/weather-search.js
+++ src/layouts/weather-search.js
@@ -2,14 +2,17 @@ class WeatherSearch {
   static displayWeatherSearchSection = (rootElement) => {
     const weatherElt = `
     <main class="main-page width-100 height-100">
-      <section class="homepage-section width-100 padding-2 centered-h-v">
-        <div class="black-transparent-bg padding-5 padding-lg-half width-80 centered-horizontal black-text rounded-corners-1 box-with-shadows">
+      <section class="homepage-section width-100 padding-2 centered-h-v"> 
+        <div class="white-transparent-bg padding-lg-half width-80 centered-horizontal black-text rounded-corners-1 box-with-shadows">
+          <h1 class="centered-text black-text margin-2">Weather App: Get weather info from anywhere on the world!!!</h1>     
           <form class="width-90 centered-horizontal">
-            <input class="width-80 rounded-corners-half" type="text" id="location-input-field">
+            <input placeholder="Enter the name of the city here... " class="width-80 rounded-corners-half" type="text" id="location-input-field">
             <button class="button-primary white-text width-10" type="button">Search</button>
           </form>
-          <div class="search-result width-60 rounded-corners-1" id="search-result"></div>
-        </div>
+          <div class="search-result width-60 rounded-corners-1 white-text padding-2" id="search-result">
+          
+          </div>
+      </div>
       </section>
     </main>
     `;
