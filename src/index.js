db548371825d4bed186b5cd26f0e42d84724ee60
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-21 12:15:23 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const rootElt = document.querySelector('#root');
+rootElt.innerHTML = 'Hello there! Welcome to the new world';
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+require('dotenv').config();
+
+module.exports = (env) => ({
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  mode: env ? 'production' : 'development',
+  devServer: {
+    inline: true,
+    port: process.env.PORT,
+    contentBase: path.join(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg|gif|jpeg)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'ToDo App',
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  performance: {
+    hints: false,
+  },
+});
