8ff01aa70ad02e2f62dab71fc6e2d503f0d1b299
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-21 14:47:31 +0200

    Style the search section

--- src/layouts/weather-search.js
+++ src/layouts/weather-search.js
@@ -1,13 +1,17 @@
 class WeatherSearch {
   static displayWeatherSearchSection = (rootElement) => {
     const weatherElt = `
-    <div>
-      <form>
-        <input type="text" id="location-input-field">
-        <button type="button">Search</button>
-      </form>
-      <div class="search-result" id="search-result"></div>
-    </div>
+    <main class="main-page width-100 height-100">
+      <section class="homepage-section width-100 padding-2 centered-h-v">
+        <div class="black-transparent-bg padding-5 padding-lg-half width-80 centered-horizontal black-text rounded-corners-1 box-with-shadows">
+          <form class="width-90 centered-horizontal">
+            <input class="width-80 rounded-corners-half" type="text" id="location-input-field">
+            <button class="button-primary white-text width-10" type="button">Search</button>
+          </form>
+          <div class="search-result width-60 rounded-corners-1" id="search-result"></div>
+        </div>
+      </section>
+    </main>
     `;
 
     rootElement.insertAdjacentHTML('beforeend', weatherElt);
