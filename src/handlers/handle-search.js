bce4bd38df8ba21ac68611f3c93e70d6aa5800ef
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-22 12:35:01 +0200

    Add return statement

--- src/handlers/handle-search.js
+++ src/handlers/handle-search.js
@@ -5,7 +5,38 @@ const handleSearch = async (inputField) => {
     { mode: 'cors' },
   );
   const resJson = await response.json();
-  console.log(resJson);
+  // console.log(resJson);
+  const {
+    feels_like: realFeel,
+    humidity,
+    pressure,
+    temp,
+    temp_max: maxTemp,
+    temp_min: minTemp,
+  } = resJson.main;
+  const { name } = resJson;
+  const {
+    country,
+    sunrise,
+    sunset,
+  } = resJson.sys;
+  const [details] = resJson.weather;
+  const { main, description, icon } = details;
+  return {
+    realFeel,
+    humidity,
+    pressure,
+    temp,
+    maxTemp,
+    minTemp,
+    name,
+    country,
+    sunrise,
+    sunset,
+    main,
+    description,
+    icon,
+  };
 };
 
 export default handleSearch;
